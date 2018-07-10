1c38680a1aacae0b4b24b22459965940ab44d64a
Navjot Kukreja <navjotk@gmail.com>
2018-07-10 15:26:18 +0100

    Add repocopy

---
+++ repocopy.sh
@@ -0,0 +1,19 @@
+mkdir inrepo
+cd inrepo
+git clone $1
+INDIR=$(ls -d */|head -n 1)
+cd ..
+mkdir outrepo
+cd outrepo
+git clone $2
+OUTDIR=$(ls -d */|head -n 1)
+cd ..
+mkdir nbdir
+cd nbdir
+git clone https://github.com/navjotk/nbparse
+cd nbparse
+python smartcopy.py $INDIR $OUTDIR
+cd $OUTDIR
+git add .
+git commit -a -m"Onward"
+git push origin master
--- smartcopy.py
+++ smartcopy.py
@@ -4,24 +4,17 @@ import shutil
 from nbparse import nbparse
 
 
-parser = argparse.ArgumentParser(description='Parse jupyter notebooks to remove cells programmatically')
-parser.add_argument('indir', type=str, nargs=1, help='name of notebook file to parse')
-parser.add_argument('outdir', type=str, nargs=1, help='name to write output file as')
 
-args = parser.parse_args()
+def smart_copy(indir, outdir):
+    pathsep = "/"
 
-indir = args.indir[0]
-outdir = args.outdir[0]
+    # Create output directory if does not exist already
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
 
-pathsep = "/"
-
-# Create output directory if does not exist already
-if not os.path.isdir(outdir):
-    os.makedirs(outdir)
-
-# Walk through every file in indir
-for fullname in os.listdir(indir):
-    filename, file_extension = os.path.splitext(fullname)
+    # Walk through every file in indir
+    for fullname in os.listdir(indir):
+        filename, file_extension = os.path.splitext(fullname)
 
     if file_extension == ".ipynb":
         nbparse(indir + pathsep + fullname, outdir + pathsep + fullname, magic_comment="OKPY_SOLUTION")
@@ -30,5 +23,18 @@ for fullname in os.listdir(indir):
             shutil.copytree(indir + pathsep + fullname, outdir + pathsep + fullname)
         else:
             shutil.copyfile(indir + pathsep + fullname, outdir + pathsep + fullname)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parse jupyter notebooks to remove cells programmatically')
+    parser.add_argument('indir', type=str, nargs=1, help='name of notebook file to parse')
+    parser.add_argument('outdir', type=str, nargs=1, help='name to write output file as')
+
+    args = parser.parse_args()
+
+    indir = args.indir[0]
+    outdir = args.outdir[0]
+    smart_copy(indir, outdir)
+
+
         
 
